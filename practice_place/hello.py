870d4e7c0bfedb3b9ffb7e35d4e892b88f6afad7
= <austin60616@gmail.com>
2022-10-19 02:06:35 +0800

    some minor update

--- practice_place/hello.py
+++
@@ -1,7 +0,0 @@
-//*[@id="__next"]/div[2]/div[2]/div/div/div/div[2]/div[2]/div/div/div[1]/div[2]/article/h2/a
-//*[@id="__next"]/div[2]/div[2]/div/div/div/div[2]/div[2]/div/div/div[1]/div[7]/div/div/div[1]/a[1]
-//*[@id="__next"]/div[2]/div[2]/div/div/div/div[2]/div[2]/div/div/div[1]/div[10]/article/h2/a
-
-//*[@id="__next"]/div[2]/div[2]/div/div/div/div[2]/div[2]/div/div/div[1]/div[22]/article/h2/a
-
-//*[@id="__next"]/div[2]/div[2]/div/div/div/div[2]/div[2]/div/div/div[1]/div[23]/article/h2/a
